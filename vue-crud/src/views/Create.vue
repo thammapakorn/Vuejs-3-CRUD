1be1a46f676cf9144b6fbb76b10483673b7c85aa
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-01 22:02:26 +0700

    binding ตัวแปร  กับ state
    import { ref } from "vue"; ประกาศ state
    
    ผูก ตัวแปร กับ state
    const fname = ref('')
    นำไปผูกกับ input       <q-input v-model="fname" label="First Name" />

--- vue-crud/src/views/Create.vue
+++ vue-crud/src/views/Create.vue
@@ -1,23 +1,22 @@
 <template>
-    <div class="q-pa-md" style="max-width: t00px">
-      <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md">
-
-      <q-input label="First Name" />
+  <div class="q-pa-md" style="max-width: t00px">
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input v-model="fname" label="First Name" />
       <q-input label="Last Name" />
       <q-input label="Username" />
       <q-input label="Password" />
       <q-input label="Email" />
       <q-input label="Avatar" />
+      <q-btn label="Submit" type="submit" color="primary" />
     </q-form>
-    </div>
-
+  </div>
 </template>
 
 <script setup>
-    const onSubmit = () =>{
+import { ref } from "vue";
 
-    }
+const fname = ref('')
+const onSubmit = () => {
+    alert(fname.value)
+};
 </script>
