70778910bacad18ecffa7c8fc42423d113b9757f
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-01 18:44:50 +0700

    เพิ่ม ปุ่ม edit - delete ให้แสดงใน column

--- vue-crud/src/views/HomeView.vue
+++ vue-crud/src/views/HomeView.vue
@@ -5,7 +5,21 @@
       :rows="rows"
       :columns="columns"
       row-key="name"
-    />
+    >
+    <template v-slot:body-cell-avatar="props">
+      <q-td :props="props">
+        <q-img
+          :src="props.row.avatar"
+        />
+      </q-td>
+      </template>
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn icon="mode_edit" @click="onEdit(props.row.id)"/>
+          <q-btn icon="delete" @click="onDelete(props.row.id)"/>
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -18,9 +32,10 @@ const columns = ref([
   { name: 'fname', label: 'left', label: 'fname', field: 'fname', sortable: true},
   { name: 'lname', label: 'left', label: 'lname', field: 'lname', sortable: true},
   { name: 'username', label: 'left', label: 'username', field: 'username', sortable: true},
-  { name: 'avatar', label: 'center', label: 'avatar', field: 'avatar'}
+  { name: 'avatar', label: 'center', label: 'avatar', field: 'avatar'},
+  { name: 'actions', align: 'center', label: 'ID', field: 'id', sortable: true }
 ])
-
+  
 const rows = ref([])
 const fetchData = () =>{
   fetch("https://www.melivecode.com/api/users")
