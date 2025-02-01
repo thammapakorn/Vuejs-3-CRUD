590ee13051ba03c1a567e7d3edd086a2c486c67e
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-02 00:42:43 +0700

    ผูก state กับตัวแปร
    ใส่ค่าเริ่มต้นจาก api จะได้ไม่ต้องกรอกเอง

--- vue-crud/src/views/Create.vue
+++ vue-crud/src/views/Create.vue
@@ -2,11 +2,11 @@
   <div class="q-pa-md" style="max-width: t00px">
     <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
       <q-input v-model="fname" label="First Name" />
-      <q-input label="Last Name" />
-      <q-input label="Username" />
-      <q-input label="Password" />
-      <q-input label="Email" />
-      <q-input label="Avatar" />
+      <q-input v-model="lname" label="Last Name" />
+      <q-input v-model="username" label="Username" />
+      <q-input v-model="password" label="Password" />
+      <q-input v-model="email" label="Email" />
+      <q-input v-model="avatar" label="Avatar" />
       <q-btn label="Submit" type="submit" color="primary" />
     </q-form>
   </div>
@@ -15,7 +15,14 @@
 <script setup>
 import { ref } from "vue";
 
-const fname = ref('')
+// ข้อมูล API จาก https://www.melivecode.com/
+const fname = ref('Cat')
+const lname = ref('Chat')
+const username = ref('cat.chat@melivecode.com')
+const password = ref('1234')
+const email = ref('cat.chat@melivecode.com')
+const avatar = ref('https://www.melivecode.com/users/cat.png')
+
 const onSubmit = () => {
     alert(fname.value)
 };
