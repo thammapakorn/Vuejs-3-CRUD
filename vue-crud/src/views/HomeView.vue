794f2897d23c6c2e17100194ee892c9220254468
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-02 15:46:04 +0700

    ส่วนของ delete
    ใช้ postman ส่ง request ไปที่ https://www.melivecode.com/api/users/delete
    โดยอ้ายอิงจาก id เมื่อกดปุ่มจะลบทันที

--- vue-crud/src/views/HomeView.vue
+++ vue-crud/src/views/HomeView.vue
@@ -1,79 +1,112 @@
 <template>
   <div class="q-pa-md">
     <div class="q-py-md">
-      <q-btn icon="add" @click="onCreate"/>
+      <q-btn icon="add" @click="onCreate" />
     </div>
-    <q-table
-      title="Treats"
-      :rows="rows"
-      :columns="columns"
-      row-key="name"
-    >
-    <template v-slot:body-cell-avatar="props">
-      <q-td :props="props">
-        <q-img
-          :src="props.row.avatar"
-        />
-      </q-td>
+    <q-table title="Treats" :rows="rows" :columns="columns" row-key="name">
+      <template v-slot:body-cell-avatar="props">
+        <q-td :props="props">
+          <q-img :src="props.row.avatar" />
+        </q-td>
       </template>
       <template v-slot:body-cell-actions="props">
         <q-td :props="props">
-          <q-btn icon="mode_edit" @click="onEdit(props.row.id)"/>
-          <q-btn icon="delete" @click="onDelete(props.row.id)"/>
+          <q-btn icon="mode_edit" @click="onEdit(props.row.id)" />
+          <q-btn icon="delete" @click="onDelete(props.row.id)" />
         </q-td>
       </template>
     </q-table>
   </div>
 </template>
 
-
 <script setup>
-import { ref } from 'vue';
-import router from '@/router';
+import { ref } from "vue";
+import router from "@/router";
 
 const columns = ref([
   // แก้หัว column
-  { name: 'id', align: 'left', label: 'ID', field: 'id', sortable: true },
-  { name: 'fname', label: 'left', label: 'fname', field: 'fname', sortable: true},
-  { name: 'lname', label: 'left', label: 'lname', field: 'lname', sortable: true},
-  { name: 'username', label: 'left', label: 'username', field: 'username', sortable: true},
-  { name: 'avatar', label: 'center', label: 'avatar', field: 'avatar'},
-  { name: 'actions', align: 'center', label: 'ID', field: 'id', sortable: true }
-])
-  
-const rows = ref([])
-const fetchData = () =>{
+  { name: "id", align: "left", label: "ID", field: "id", sortable: true },
+  {
+    name: "fname",
+    label: "left",
+    label: "fname",
+    field: "fname",
+    sortable: true,
+  },
+  {
+    name: "lname",
+    label: "left",
+    label: "lname",
+    field: "lname",
+    sortable: true,
+  },
+  {
+    name: "username",
+    label: "left",
+    label: "username",
+    field: "username",
+    sortable: true,
+  },
+  { name: "avatar", label: "center", label: "avatar", field: "avatar" },
+  {
+    name: "actions",
+    align: "center",
+    label: "ID",
+    field: "id",
+    sortable: true,
+  },
+]);
+
+const rows = ref([]);
+const fetchData = () => {
   fetch("https://www.melivecode.com/api/users")
-  .then(res => res.json())
-  .then((result)=>{
-    rows.value = result;
-  })
-}
-fetchData()
+    .then((res) => res.json())
+    .then((result) => {
+      rows.value = result;
+    });
+};
+fetchData();
 
-const onEdit = (id) =>{
-  alert(id+"edit")
-}
-const onDelete = (id) =>{
-  alert(id+"delete")
-}
-const onCreate = () =>{
-  router.push('/create')
-}
-  // ลำดับของตัวแปรต้องตรงกัน
-  // {"id":1,"fname":"Karn","lname":"Yong","username":"karn.yong@melivecode.com","avatar":"https://www.melivecode.com/users/1.png"},
-  // {"id":2,"fname":"Ivy","lname":"Cal","username":"ivy.cal@melivecode.com","avatar":"https://www.melivecode.com/users/2.png"},
-  // {"id":3,"fname":"Walter","lname":"Beau","username":"walter.beau@melivecode.com","avatar":"https://www.melivecode.com/users/3.png"},
-  // {"id":4,"fname":"Gayla","lname":"Bertrand","username":"gayla.bertrand@melivecode.com","avatar":"https://www.melivecode.com/users/4.png"},
-  // {"id":5,"fname":"Benjamin","lname":"Chaz","username":"benjamin.chaz@melivecode.com","avatar":"https://www.melivecode.com/users/5.png"},
-  // {"id":6,"fname":"Delia","lname":"Robin","username":"delia.robin@melivecode.com","avatar":"https://www.melivecode.com/users/6.png"},
-  // {"id":7,"fname":"Hector","lname":"Graves","username":"hector.graves@melivecode.com","avatar":"https://www.melivecode.com/users/7.png"},
-  // {"id":8,"fname":"Diego","lname":"Greene","username":"diego.greene@melivecode.com","avatar":"https://www.melivecode.com/users/8.png"},
-  // {"id":9,"fname":"Izzie","lname":"Mooney","username":"izzie.mooney@melivecode.com","avatar":"https://www.melivecode.com/users/9.png"},
-  // {"id":10,"fname":"Romeo","lname":"Garner","username":"romeo.garner@melivecode.com","avatar":"https://www.melivecode.com/users/10.png"} 
+const onEdit = (id) => {
+  alert(id + "edit");
+};
+const onDelete = (id) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
+  const raw = JSON.stringify({
+    id: id,
+  });
+
+  const requestOptions = {
+    method: "DELETE",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://www.melivecode.com/api/users/delete", requestOptions)
+    .then((response) => response.json())
+    .then(result => {
+      alert(result.message)
+      fetchData()
+    })
+    .catch((error) => console.error(error));
+};
+const onCreate = () => {
+  router.push("/create");
+};
+// ลำดับของตัวแปรต้องตรงกัน
+// {"id":1,"fname":"Karn","lname":"Yong","username":"karn.yong@melivecode.com","avatar":"https://www.melivecode.com/users/1.png"},
+// {"id":2,"fname":"Ivy","lname":"Cal","username":"ivy.cal@melivecode.com","avatar":"https://www.melivecode.com/users/2.png"},
+// {"id":3,"fname":"Walter","lname":"Beau","username":"walter.beau@melivecode.com","avatar":"https://www.melivecode.com/users/3.png"},
+// {"id":4,"fname":"Gayla","lname":"Bertrand","username":"gayla.bertrand@melivecode.com","avatar":"https://www.melivecode.com/users/4.png"},
+// {"id":5,"fname":"Benjamin","lname":"Chaz","username":"benjamin.chaz@melivecode.com","avatar":"https://www.melivecode.com/users/5.png"},
+// {"id":6,"fname":"Delia","lname":"Robin","username":"delia.robin@melivecode.com","avatar":"https://www.melivecode.com/users/6.png"},
+// {"id":7,"fname":"Hector","lname":"Graves","username":"hector.graves@melivecode.com","avatar":"https://www.melivecode.com/users/7.png"},
+// {"id":8,"fname":"Diego","lname":"Greene","username":"diego.greene@melivecode.com","avatar":"https://www.melivecode.com/users/8.png"},
+// {"id":9,"fname":"Izzie","lname":"Mooney","username":"izzie.mooney@melivecode.com","avatar":"https://www.melivecode.com/users/9.png"},
+// {"id":10,"fname":"Romeo","lname":"Garner","username":"romeo.garner@melivecode.com","avatar":"https://www.melivecode.com/users/10.png"}
 </script>
 
-<style>
-
-</style>
+<style></style>
